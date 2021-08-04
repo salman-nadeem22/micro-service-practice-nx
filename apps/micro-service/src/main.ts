58de7db33d3bb70f12f8a1518fa7ba57224c8bee
Salman Nadeem <65226911+salman-nadeem22@users.noreply.github.com>
2021-08-04 10:51:01 +0500

    Created micro-service with tcp transport

--- apps/micro-service/src/main.ts
+++ apps/micro-service/src/main.ts
@@ -5,16 +5,22 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 
 import { AppModule } from './app/app.module';
 
+const logger = new Logger('Main');
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: '127.0.0.1',
+      port: 8877,
+    },
+  });
+
+  await app.listen(() => {
+    logger.log('Listening from micro service');
   });
 }
 
